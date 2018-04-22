0794e6ec5e424076ecbce0eff17d6891aedea3f9
wmarttala <wmarttala123@gmail.com>
2018-04-22 00:24:15 -0500

    Update lib.js

--- src/lib.js
+++ src/lib.js
@@ -1,11 +1,10 @@
-// TODO copy readFile() which uses Promises
-
-// TODO copy writeFile() which uses Promises
-
 // TODO copy chooseRandom() from previous assignment
+const chooseRandom = () => {} 
 
 // TODO copy createPrompt() from previous assignment
+const createPrompt = () => {}
 
 // TODO implement createQuestions()
+const createQuestions = () => {}
 
 // TODO export above functions
